222e87aa83ceae266ab5e022d034ca224f750f4b
Daniel-R-V <danirvicente@gmail.com>
2022-03-04 00:14:36 +0100

    adding board and its style

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,45 @@
+import "./header.scss";
+
 function Layout({ user }) {
   return (
     <div>
       {!user ? (
         <p>You need to be logged to use the app</p>
       ) : (
-        <p>The connected user is {user.name}</p>
+        <div className="board">
+          <div className="toDo">
+            <div>
+              <h3>TO DO</h3>
+            </div>
+            <div>
+              <p>Header design</p>
+              <p>Column dinamic moves</p>
+              <p>Improve soft skills</p>
+            </div>
+          </div>
+          <div className="inProgress">
+            <div>
+              <h3>IN PROGRESS</h3>
+            </div>
+            <div>
+              <p>Doing project</p>
+              <p>Learning React</p>
+              <p>Learning HTML</p>
+              <p>Learning SASS</p>
+            </div>
+          </div>
+          <div className="done">
+            <div>
+              <h3>DONE</h3>
+            </div>
+            <div>
+              <p>Finishing DAW</p>
+              <p>Working hard</p>
+              <p>Web Development</p>
+              <p></p>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
