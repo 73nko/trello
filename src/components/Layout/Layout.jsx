4394a609a1f8bf38f0dd289ef1ce9d091631d4ea
Daniel-R-V <danirvicente@gmail.com>
2022-03-04 10:26:41 +0100

    changing column style

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,37 +6,43 @@ function Layout({ user }) {
       {!user ? (
         <p>You need to be logged to use the app</p>
       ) : (
-        <div className="board">
-          <div className="toDo">
-            <div>
-              <h3>TO DO</h3>
-            </div>
-            <div>
-              <p>Header design</p>
-              <p>Column dinamic moves</p>
-              <p>Improve soft skills</p>
-            </div>
-          </div>
-          <div className="inProgress">
-            <div>
-              <h3>IN PROGRESS</h3>
-            </div>
-            <div>
-              <p>Doing project</p>
-              <p>Learning React</p>
-              <p>Learning HTML</p>
-              <p>Learning SASS</p>
+        <div id="board">
+          <div className="column">
+            <div className="toDo">
+              <div>
+                <h3>TO DO</h3>
+              </div>
+              <div>
+                <p>Header design</p>
+                <p>Column dinamic moves</p>
+                <p>Improve soft skills</p>
+              </div>
             </div>
           </div>
-          <div className="done">
-            <div>
-              <h3>DONE</h3>
+          <div className="column">
+            <div className="inProgress">
+              <div>
+                <h3>IN PROGRESS</h3>
+              </div>
+              <div>
+                <p>Doing project</p>
+                <p>Learning React</p>
+                <p>Learning HTML</p>
+                <p>Learning SASS</p>
+              </div>
             </div>
-            <div>
-              <p>Finishing DAW</p>
-              <p>Working hard</p>
-              <p>Web Development</p>
-              <p></p>
+          </div>
+          <div className="column">
+            <div className="done">
+              <div>
+                <h3>DONE</h3>
+              </div>
+              <div>
+                <p>Finishing DAW</p>
+                <p>Working hard</p>
+                <p>Web Development</p>
+                <p></p>
+              </div>
             </div>
           </div>
         </div>
