112130143099f47543713886ad8e267a85364c33
Daniel-R-V <danirvicente@gmail.com>
2022-03-06 01:21:31 +0100

    new style app

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -8,27 +8,28 @@ function Layout({ user }) {
       ) : (
         <div id="board">
           <div className="column">
-            <div className="toDo">
+            <div className="todo">
               <div>
                 <h3>TO DO</h3>
               </div>
               <div>
-                <p>Header design</p>
-                <p>Column dinamic moves</p>
-                <p>Improve soft skills</p>
+                {/* Todas estas "tareas" entraran como parrafo desde html? cual seria la etiqueta mas eficiente? */}
+                <div className="tareas">Header design</div>
+                <div className="tareas">Column dinamic moves</div>
+                <div className="tareas">Improve soft skills</div>
               </div>
             </div>
           </div>
           <div className="column">
-            <div className="inProgress">
+            <div className="inprogress">
               <div>
                 <h3>IN PROGRESS</h3>
               </div>
               <div>
-                <p>Doing project</p>
-                <p>Learning React</p>
-                <p>Learning HTML</p>
-                <p>Learning SASS</p>
+                <div className="tareas">Doing project</div>
+                <div className="tareas">Learning React</div>
+                <div className="tareas">Learning HTML</div>
+                <div className="tareas">Learning SASS</div>
               </div>
             </div>
           </div>
@@ -38,10 +39,9 @@ function Layout({ user }) {
                 <h3>DONE</h3>
               </div>
               <div>
-                <p>Finishing DAW</p>
-                <p>Working hard</p>
-                <p>Web Development</p>
-                <p></p>
+                <div className="tareas">Finishing DAW</div>
+                <div className="tareas">Working hard</div>
+                <div className="tareas">Web Development</div>
               </div>
             </div>
           </div>
