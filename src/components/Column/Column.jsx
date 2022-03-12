8cea9c7697ca1df0054698f3a76f63d27850b81c
Daniel <danirvicente@gmail.com>
2022-03-12 12:15:54 +0100

    update column component and tasks align

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,18 +1,31 @@
 import "./column.css";
+
+const ColumnTitle = ({ title }) => {
+  return (
+    <header>
+      <h3>{title}</h3>
+    </header>
+  );
+};
+
+//Como podemos diferenciar las tareas de cada columna? en este momento tenemos 3 tareas iguales para las 3 columnas
+
+const TasksColumn = () => {
+  return (
+    <ul>
+      <li className="tasks">Header design</li>
+      <li className="tasks">Column dinamic moves</li>
+      <li className="tasks">Improve soft skills</li>
+    </ul>
+  );
+};
+
 const Column = ({ title }) => {
   return (
     <section className="column">
-      <header>
-        <h3>{title}</h3>
-      </header>
-      <ul>
-        <li className="tasks">Header design</li>
-        <li className="tasks">Column dinamic moves</li>
-        <li className="tasks">Improve soft skills</li>
-      </ul>
+      <ColumnTitle title={title} />
+      <TasksColumn />
     </section>
   );
 };
 export default Column;
-
-// crear como funciones header y ul
